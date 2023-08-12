bc54b39c36782668d4cd54f8d9d674b56f9ac4f3
unknown <David.coh19@gmail.com>
2023-08-12 01:46:32 -0400

    Updates

---
+++ _next/static/21ftka3dFr_-0KyPrCbzd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-ada720411946ba58.js","static/chunks/pages/index-c2dba61ec41e02a1.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
