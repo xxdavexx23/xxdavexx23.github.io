3f321b9471c54cd1c319f086abbfb3986f90bf8c
David Isaac Cohen <dc@Davids-Air-1293.lan>
2023-12-17 19:56:11 -0500

    continued change

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/xxdavexx23.github.io',
-    assetPrefix: '/xxdavexx23.github.io/',
 }
 
 module.exports = nextConfig
