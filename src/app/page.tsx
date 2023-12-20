0d228915e5194e4bc6c6f961f30723f7863a5d21
xxdavexx23 <70821018+xxdavexx23@users.noreply.github.com>
2023-12-20 17:31:20 -0500

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1> HELLO </h1>
     </main>
   )
