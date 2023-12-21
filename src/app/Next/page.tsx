8d8f457b15ae8dd4b27ae419173ba392b8c72331
xxdavexx23 <70821018+xxdavexx23@users.noreply.github.com>
2023-12-21 14:19:35 -0500

    a

--- src/app/Next/page.tsx
+++ src/app/Next/page.tsx
@@ -1,5 +1,5 @@
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-    return <h1 className="flex min-h-screen flex-col items-center justify-between p-24">
+    return <h1 className="flex min-h-screen flex-col items-center justify-between">
         Hello, Dashboard Page!</h1>
 }
