413308d5c7d95a6de50ab5d3020eb7fdf7169746
xxdavexx23 <70821018+xxdavexx23@users.noreply.github.com>
2023-12-21 20:19:22 +0000

    router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image'
-
+import { useRouter } from 'next/navigation'
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <main className="main-content">
       <h1> HELLO </h1>
+      <button type="button" onClick={() => router.push('/Next')}>
+      Dashboard
+    </button>
     </main>
   )
+
 }
