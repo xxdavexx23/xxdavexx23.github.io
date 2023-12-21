151dabe9f1514adc87d7805adf4d984deb385293
David Isaac Cohen <dc@Davids-MacBook-Air-1293.local>
2023-12-20 23:36:30 -0500

    added styling

--- src/app/Next/page.tsx
+++ src/app/Next/page.tsx
@@ -1,4 +1,5 @@
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-    return <h1>Hello, Dashboard Page!</h1>
+    return <h1 className="flex min-h-screen flex-col items-center justify-between p-24">
+        Hello, Dashboard Page!</h1>
 }
