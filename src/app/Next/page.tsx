de713806662dc29cb94fb9f5ed85c0b549f0661f
xxdavexx23 <70821018+xxdavexx23@users.noreply.github.com>
2023-12-21 14:45:47 -0500

    remove tailwind

--- src/app/Next/page.tsx
+++ src/app/Next/page.tsx
@@ -1,5 +1,5 @@
 // `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
 export default function Page() {
-    return <h1 className="flex min-h-screen flex-col items-center justify-between">
+    return <h1 className="flex min-h-screen flex-col items-center justify-between p-24">
         Hello, Dashboard Page!</h1>
 }
