b9225c6c0875269063eb13fee83373f01cf0ca4b
David Isaac Cohen <dc@Davids-MacBook-Air-1293.local>
2023-12-22 17:22:48 -0500

    home page modifications

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,19 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'David Cohen',
   description: 'Welcome to my Website',
-}
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <h1>HELLO Im in Layout</h1>
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <div className={inter.className}>
+      <h1>HELLO, I'm in Layout</h1>
+      {children}
+    </div>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 
 'use client'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link';
+
+
 
 export default function Home() {
   const router = useRouter();
   return (
     <main className="main-content">
-      <h1> HELLO </h1>
-      <button type="button" onClick={() => router.push('/Next')}>
-      Dashboard
-      </button>
-      <h1>what?</h1>
+      <div className="floating-nav">
+        <ul>
+          <Link href="/Next">About Page</Link>
+        </ul>
+      </div>
     </main>
   )
 
