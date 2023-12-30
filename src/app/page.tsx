78a7c192b75d4248f16e67b6ac5cb278d98e1a7b
Mostafa Aldilati <mustafa_kjm@hotmail.com>
2023-12-29 22:55:46 -0500

    Updated HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,61 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect, useRef } from 'react';
 import { SquareAvatar } from '@/components/Avatar';
 import styles from './Home.module.css';
 
 export default function Home() {
+  const [title, setTitle] = useState('');
+  const titles = ['THINKER',  'DESIGNER', 'ENGINEER'];
+
+  const currentTitleIndex = useRef(0);
+  const charIndex = useRef(0);
+  const isDeleting = useRef(false);
+
+  useEffect(() => {
+    const typeTitle = () => {
+      const currentTitle = titles[currentTitleIndex.current];
+      const updateSpeed = isDeleting.current ? 100 : 200; // Speed of typing or deleting
+
+      if (isDeleting.current) {
+        setTitle(currentTitle.substring(0, charIndex.current--));
+      } else {
+        setTitle(currentTitle.substring(0, charIndex.current++));
+      }
+
+      if (!isDeleting.current && charIndex.current === currentTitle.length) {
+        // Pause before start deleting
+        setTimeout(() => { isDeleting.current = true; }, 1500);
+      } else if (isDeleting.current && charIndex.current === 0) {
+        isDeleting.current = false;
+        currentTitleIndex.current = (currentTitleIndex.current + 1) % titles.length;
+        // Pause before start typing next title
+        setTimeout(typeTitle, 1500);
+        return;
+      }
+
+      setTimeout(typeTitle, updateSpeed);
+    };
+
+    const typingTimer = setTimeout(typeTitle, 200); // Initial delay for typing effect
+
+    // Clean up function to clear the timeout
+    return () => clearTimeout(typingTimer);
+  }, []);
+
   return (
     <main className={styles.container}>
-      <div className={styles.content}>
+      <section className={styles.introSection}>
+        <div className="flex flex-col justify-center">
+          <h1 className={styles.title}>HI THERE, I AM DAVID! <br/> <span className={styles.typing}>{title}</span></h1>
+          <p className={styles.subtitle}>
+            based in Orlando, FL
+          </p>
+          <button className={styles.contactButton}>Contact Me</button>
+        </div>
         <div className={styles.avatar}>
           <SquareAvatar />
         </div>
-
-        <div className={styles.header}>
-          <h1 className={styles.title}>Hi! I am David!</h1>
-          <p className={styles.subtitle}>
-            Software Engineer at Northrop Grumman with extensive experience in both backend and frontend development.
-          </p>
-        </div>
-      </div>
+      </section>
     </main>
   );
 }
--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,12 @@
 import Image from "next/image";
-
+import david from '../assets/david.png'
 export const SquareAvatar = () => {
   return (
     <Image
-      src="https://source.unsplash.com/120x120/?portrait"
-      className="rounded-lg w-32 h-32"
+      src={david}
       alt="Avatar"
       width={400}
-      height={400}
+      height={500}
     />
   );
 };
