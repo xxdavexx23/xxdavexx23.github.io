d7c7e91146d252ded9d3c5ac2733e1e422a66870
Mostafa <mustafa_kjm@hotmail.com>
2024-01-05 00:01:54 -0500

    Updated About Me Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client';
-import React, { useState, useEffect, useRef } from 'react';
-import { SquareAvatar } from '@/components/Avatar';
-import styles from './Home.module.css';
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import { SquareAvatar } from "@/components/Avatar";
+import styles from "./Home.module.css";
 
 export default function Home() {
-  const [title, setTitle] = useState('');
-  const titles = ['THINKER', 'DESIGNER', 'ENGINEER'];
+  const [title, setTitle] = useState("");
+  const titles = ["THINKER", "DESIGNER", "ENGINEER"];
 
   const currentTitleIndex = useRef(0);
   const charIndex = useRef(0);
@@ -24,10 +24,13 @@ export default function Home() {
 
       if (!isDeleting.current && charIndex.current === currentTitle.length) {
         // Pause before start deleting
-        setTimeout(() => { isDeleting.current = true; }, 1500);
+        setTimeout(() => {
+          isDeleting.current = true;
+        }, 1500);
       } else if (isDeleting.current && charIndex.current === 0) {
         isDeleting.current = false;
-        currentTitleIndex.current = (currentTitleIndex.current + 1) % titles.length;
+        currentTitleIndex.current =
+          (currentTitleIndex.current + 1) % titles.length;
         // Pause before start typing next title
         setTimeout(typeTitle, 1500);
         return;
@@ -46,24 +49,86 @@ export default function Home() {
     <main className={styles.container}>
       <section className={styles.introSection}>
         <div className="flex flex-col justify-center">
-          <h1 className={styles.title}>HI THERE, I AM DAVID! <br /> <span className={styles.typing}>{title}</span></h1>
-          <p className={styles.subtitle}>
-            based in Orlando, FL
-          </p>
+          <h1 className={styles.title}>
+            HI THERE, I AM DAVID! <br />{" "}
+            <span className={styles.typing}>{title}</span>
+          </h1>
+          <p className={styles.subtitle}>based in Orlando, FL</p>
           <button className={styles.contactButton}>Contact Me</button>
         </div>
         <div className={styles.avatar}>
           <SquareAvatar />
         </div>
+        <div className={styles.jumpingArrow}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M12 5v14M19 12l-7 7-7-7"></path>
+          </svg>
+        </div>
       </section>
-      <section id="about">
-        <h1> Hello </h1>
-      </section>
-      <section id="projects">
-        Hello
-      </section>
-      <section id="articles">
-        Hello
+
+      <section className={styles.aboutSection}>
+        <div className={styles.aboutContent}>
+          <div className={styles.aboutHeader}>
+            <h2 className={styles.aboutSubHeading}>About Me</h2>
+            <h2 className={styles.aboutHeading}>Know Me More</h2>
+          </div>
+          <div className={styles.aboutBody}>
+            <div className={styles.aboutMe}>
+              <h1 className={styles.aboutTitle}>
+                Hi, I'm{" "}
+                <span className={styles.nameHighlight}>David Cohen</span>
+              </h1>
+              <p className={styles.aboutText}>
+                <br />
+                I'm a dedicated software engineer at Northrop, specializing in
+                Full Stack Development and Software Testing and Engineering. My
+                journey includes obtaining a master's degree in Electrical
+                Engineering from UCF, equipping me with a wealth of knowledge
+                and expertise. I am committed to developing innovative solutions
+                and delivering high-quality software that not only meets but
+                exceeds client expectations. Let's collaborate and bring your
+                ideas to life together.
+              </p>
+            </div>
+            <div className={styles.experience}>
+              <span className={styles.experienceNumber}>02</span>
+              <span className={styles.experienceText}>Years of Experience</span>
+            </div>
+          </div>
+
+          <div className={styles.aboutDetails}>
+            <div className={styles.detailItem}>
+              <span className={styles.detailTitle}>Name:</span>
+              <br />
+              <span className={styles.detail}>David Cohen</span>
+            </div>
+            <div className={styles.detailItem}>
+              <span className={styles.detailTitle}>Email:</span>
+              <br />
+              <span className={styles.detail}>david.coh19@gmail.com</span>
+            </div>
+            <div className={styles.detailItem}>
+              <span className={styles.detailTitle}>Date of birth:</span>
+              <br />
+              <span className={styles.detail}>30 November, 1998</span>
+            </div>
+            <div className={styles.detailItem}>
+              <span className={styles.detailTitle}>From:</span>
+              <br />
+              <span className={styles.detail}>Fort Lauderdale, USA.</span>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
